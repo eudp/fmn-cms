ade2ec385f0060531e0590568cb51ef44c952218
eudp <eudp_95@hotmail.com>
2018-01-15 21:02:28 -0400

    Updating code centros

--- application/views/centros/index.php
+++ application/views/centros/index.php
@@ -2,15 +2,15 @@
     <div class="row">
         <div class="col-12"><h2>Centros</h2></div>
 
-    <?php foreach ($establishments as $establishment_item): ?>
-        
-        <div class="col-3">
-            <h3><?= $establishment_item['acronym']; ?></h2>
-            <a href="<?= site_url('centros/'. $establishment_item['establishment_id']);?>"><?= $establishment_item['title']; ?></a>
-            <p>Image: <a href="<?= $establishment_item['path']; ?>"><?= $establishment_item['path']; ?></a></p>
-        </div>
+        <?php foreach ($establishments as $establishment_item): ?>
+            
+            <div class="col-3">
+                <h3><?= $establishment_item['acronym']; ?></h3>
+                <a href="<?= site_url('centros/'. $establishment_item['establishment_id']);?>"><?= $establishment_item['title']; ?></a>
+                <p>Image: <a href="<?= $establishment_item['path']; ?>"><?= $establishment_item['path']; ?></a></p>
+            </div>
 
-    <?php endforeach; ?>
+        <?php endforeach; ?>
     
     </div>
 </div>
