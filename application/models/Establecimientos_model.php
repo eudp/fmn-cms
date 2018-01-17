4e1824788a3569fd85d5eff6929644181e60275a
eudp <eudp_95@hotmail.com>
2018-01-17 16:49:15 -0400

    Improving establecimientos model

--- application/models/Establecimientos_model.php
+++ application/models/Establecimientos_model.php
@@ -6,21 +6,35 @@ class Establecimientos_model extends CI_Model {
         $this->load->database();
     }
 
-    public function get($type, $establishment_id = FALSE)
+    public function get($type, $establishment_id = FALSE, $status=1)
     {
-        if ($establishment_id === FALSE)
+        if ($establishment_id === false)
         {
-            $this->db->select('e.acronym, e.title, a.path, e.establishment_id');
+            $this->db->select('e.acronym, e.title, a.path, e.establishment_id, e.creation_date, e.modified_date, e.status');
             $this->db->from('establecimientos as e');
-            $this->db->join('archivos as a', 'a.file_id = e.image_id');
-            $this->db->where(array('type' => $type, 'status' => 1 ));
+            $this->db->join('archivos as a', 'a.file_id = e.image_id', 'left');
+
+            if ($status != 'all') {
+                $this->db->where('status' , $status );
+            }
+            if ($type !== 'all'){
+                $this->db->where('type' , $type );
+            }
+
             $query = $this->db->get();
             return $query->result_array();
         }
 
-        $this->db->select('e.acronym, e.title, e.description, e.address, e.phone, e.email, e.facebook_url, e.twitter_url, e.site_url, e.schedule, e.services');
+        $this->db->select('e.acronym, e.title, e.description, e.address, e.phone, e.email, e.facebook_url, e.twitter_url, e.site_url, e.schedule, e.services, e.instagram_url, e.establishment_id, e.status');
         $this->db->from('establecimientos as e');
-        $this->db->where(array('type' => $type, 'status' => 1,'establishment_id' => $establishment_id ));
+        if ($status != 'all') {
+            $this->db->where('status' , $status );
+        }
+        if ($type !== 'all'){
+            $this->db->where('type' , $type );
+        }
+
+        $this->db->where('establishment_id', $establishment_id );
         $query = $this->db->get();
         return $query->row_array();
     }
@@ -33,5 +47,12 @@ class Establecimientos_model extends CI_Model {
         $this->db->where(array('establishment_id' => $establishment_id ));
         $query = $this->db->get();
         return $query->result_array();
+    }
+
+    public function update ($array, $establishment_id)
+    {
+        $this->db->set($array);
+        $this->db->where('establishment_id', $establishment_id);
+        $this->db->update('establecimientos');
     }
 }
