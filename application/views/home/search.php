a551807f741fa8f7775345800c4dfa19e87a80ad
eudp <eudp_95@hotmail.com>
2018-01-17 01:28:43 -0400

    Adding page search

---
+++ application/views/home/search.php
@@ -0,0 +1,51 @@
+<div class="container">
+    <h3>Búsqueda</h3>
+    <div class="row">
+        <div class="col-12"><h5>Noticias</h5></div>
+        <?php foreach ($news as $news_item): ?>
+            
+            <div class="col-3">
+                <span><?= date('j \d\e F, Y', $news_item['publication_date']); ?></span>
+                <a href="<?= site_url('noticias/'. $news_item['news_id']);?>"><h5><?= $news_item['title']; ?></h5></a>
+            </div>
+
+        <?php endforeach; ?>
+    
+    </div>
+    <div class="row">
+        <div class="col-12"><h5>Multimedia</h5></div>
+        <?php foreach ($multimedia as $multimedia_item): ?>
+            
+            <div class="col-3">
+                <span><?= date('j \d\e F, Y', $multimedia_item['creation_date']); ?></span>
+                <a href="<?= site_url('multimedia/'. $multimedia_item['multimedia_id']);?>"><h5><?= $multimedia_item['title']; ?></h5></a>
+            </div>
+
+        <?php endforeach; ?>
+    
+    </div>
+    <div class="row">
+        <div class="col-12"><h5>Agenda</h5></div>
+        <?php foreach ($diary as $diary_item): ?>
+            
+            <div class="col-3">
+                <span><?= date('j \d\e F, Y', $diary_item['publication_date']); ?></span>
+                <a href="<?= site_url('agenda/'. $diary_item['diary_id']);?>"><h5><?= $diary_item['title']; ?></h5></a>
+            </div>
+
+        <?php endforeach; ?>
+    
+    </div>
+    <div class="row">
+        <div class="col-12"><h5>Exposiciones</h5></div>
+        <?php foreach ($expositions as $expositions_item): ?>
+            
+            <div class="col-3">
+                <span><?= date('j \d\e F, Y', $expositions_item['creation_date']); ?></span>
+                <a href="<?= site_url('exposiciones/'. $expositions_item['exposition_id']);?>"><h5><?= $expositions_item['title']; ?></h5></a>
+            </div>
+
+        <?php endforeach; ?>
+    
+    </div>
+</div>
