e159e77e8569fd78f361052cef79fedb85efdb92
eudp <eudp_95@hotmail.com>
2018-01-18 16:37:08 -0400

    Updating noticias model

--- application/models/Noticias_model.php
+++ application/models/Noticias_model.php
@@ -6,22 +6,24 @@ class Noticias_model extends CI_Model {
         $this->load->database();
     }
 
-    public function get($news_id = FALSE)
+    public function get($news_id = null, $status=1)
     {
-        if ($news_id === FALSE)
+        if ($news_id === null)
         {
-            $this->db->select('n.title, a.path, n.news_id, n.excerpt, n.publication_date');
+            $this->db->select('n.title, a.path, n.news_id, n.excerpt, n.publication_date, n.creation_date, n.modified_date, n.status');
             $this->db->from('noticias as n');
-            $this->db->join('archivos as a', 'a.file_id = n.image_id');
-            $this->db->where(array('status' => 1 ));
+            $this->db->join('archivos as a', 'a.file_id = n.image_id', 'left');
+            if ($status != null) {
+                $this->db->where('status' , $status );
+            }
             $this->db->order_by('publication_date', 'DESC');
             $query = $this->db->get();
             return $query->result_array();
         }
 
-        $this->db->select('n.title, a.path, n.description, n.publication_date');
+        $this->db->select('n.title, a.path, n.description, n.publication_date, n.excerpt, a.file_name, n.status, n.news_id');
         $this->db->from('noticias as n');
-        $this->db->join('archivos as a', 'a.file_id = n.image_id');
+        $this->db->join('archivos as a', 'a.file_id = n.image_id', 'left');
         $this->db->where(array('news_id' => $news_id ));
         $query = $this->db->get();
         return $query->row_array();
@@ -43,6 +45,25 @@ class Noticias_model extends CI_Model {
 
         $query = $this->db->get();
         return $query->result_array();
+    }
+
+    public function set ($array, $news_id = null)
+    {
+        if ($news_id != null) {
+            $this->db->set($array);
+            $this->db->where('news_id', $news_id);
+            $this->db->update('noticias');
+        }
+        else {
+            $this->db->insert('noticias', $array);
+
+            return $this->db->insert_id();
+        }  
+    }
+
+    public function delete ($news_id)
+    {
+        $this->db->delete('noticias', array('news_id' => $news_id));   
     }
 
 }
