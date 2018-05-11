614a09981a6ea9a1f83f3605a843e61333b4e393
Efrain Diaz <eudp_95@hotmail.com>
2018-05-11 12:09:55 -0400

    Adding name log in user in navbar

--- application/views/includes/header_admin.php
+++ application/views/includes/header_admin.php
@@ -16,15 +16,15 @@
 <body>
 	<!-- Menú principal -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="<?= site_url('');?>">
-			<img src="http://www.fmn.gob.ve/sites/default/files/logo.jpg" width="120" height="64" alt="">
-		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Hola, <?= $this->ion_auth->user()->row()->first_name ?> </a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="<?= site_url('logout');?>">Logout</a>
+				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="<?= site_url('admin/');?>">Admin </a>
 				</li>
